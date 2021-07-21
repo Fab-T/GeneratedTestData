7850bfec6c5d16a3594dc2bd93e559c77f0865ba
1FabT <fabien.traquet@gmail.com>
2021-07-21 12:54:10 -0500

    adding tests and service descriptions

--- GeneratedTestData/testDataGenerator.py
+++ GeneratedTestData/testDataGenerator.py
@@ -1,38 +1,69 @@
-#!/usr/bin/env python
-
 """
 This small program is used to generate test data matching a given format.
 The output of this program will be a list of dictionary items
 """
 import pprint
+import os
 from faker import Faker
 fake = Faker()
+
 # Global variable
-TEST_DATA_GENERATED_ITEMS = []
+filepath_service_description = "config/service_description.txt"
 
-def format(dictionary,itemMax):
+def filetolist(file_path):
+    def fileread(file_read):
+        #check is filename is a string then open file to read/put lines in a single string, then close filename.
+        if isinstance(file_read,str):
+            with open(file_read,'r') as f:
+                file_string=f.read()
+                f.close()
+        return file_string
+    # define a list of words (separated by comma)
+    list_words_separated_by_comma = fileread(file_path).split(",")
+    return list_words_separated_by_comma
+
+def format(item_dictionary,item_max):
     # remove index and create list
-    for i in range(0, itemMax):
-        TEST_DATA_GENERATED_ITEMS.append(dictionary[i])
-    #print(TEST_DATA_GENERATED_ITEMS)
-    pprint.pprint(TEST_DATA_GENERATED_ITEMS,width=1)
+    TEST_DATA_GENERATED_ITEMS = []
+    for i in range(0, item_max):
+        TEST_DATA_GENERATED_ITEMS.append(item_dictionary[i])
 
-def input_data(itemMax):
+    return TEST_DATA_GENERATED_ITEMS
+
+def print_data(item_list):
+    #use pretty print to generate requested formating
+    pprint.pprint(item_list)
+
+def input_data(item_max):
+    # generate list of service description
+    service_description_list = filetolist(filepath_service_description)
+
+
     # dictionnary
     generated_data= {}
-    for i in range(0, itemMax):
-        generated_data[i]= {}
-        generated_data[i]["client_first_name"]= fake.first_name()
-        generated_data[i]["client_last_name"]= fake.last_name()
+    for i in range(0, item_max):
+        generated_data[i] = {}
+        generated_data[i]["client_first_name"] = fake.first_name()
+        generated_data[i]["client_last_name"] = fake.last_name()
+        generated_data[i]["service_description"] = fake.words(1,service_description_list,True)[0]
     return generated_data
+
 
 def main():
 
     # Enter number of Entries
+    #TODO make this an argument of the script
     number_of_entries = 5
+
+    # Generate fake data
     generated_data = (input_data(number_of_entries))
-    # print(generated_data)
-    format(generated_data,number_of_entries)
+    # format data and print it
+    print_data(format(generated_data, number_of_entries))
+
+    # Test
+    # service_description_list = filetolist(filepath_service_description)
+    # print(format(generated_data, number_of_entries))
+
 
 if __name__ == "__main__":
     main()
---
+++ Tests/test_testDataGenerator.py
@@ -0,0 +1,24 @@
+# unit tests for the module testDataGenerator
+import os
+from GeneratedTestData import testDataGenerator
+
+# test 5 entries generates 5 items
+#TODEL input_data = target.input_data
+
+def test_generateFiveEntries():
+    five_entries = (testDataGenerator.input_data(5))
+    assert len(five_entries) == 5
+
+# test 1 entry generates client first name, client last name
+
+def test_generateformat():
+    one_entry = (testDataGenerator.format(testDataGenerator.input_data(1), 1))
+    # Using iter to parse dictionary keys with next
+    iter_one_entry = (iter(one_entry[0]))
+    assert print(next(iter_one_entry)) == "client_first_name"
+    assert print(next(iter_one_entry)) == "client_first_name"
+    assert print(next(iter_one_entry)) == "service_description"
+
+
+
+
